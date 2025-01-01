595bae939af6b8f2dd41a634ad7e5c0e935b9c8f
castlemin999 <castlemin999@gmail.com>
2025-01-01 21:54:46 +0900

    Feat: Redis 설정

--- src/main/java/com/matching/band/domain/user/controller/UserController.java
+++ src/main/java/com/matching/band/domain/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.matching.band.domain.user.controller;
 
 import com.matching.band.domain.user.dto.SignUpRequest;
 import com.matching.band.domain.user.service.UserService;
+import com.matching.band.global.config.RedisService;
 import com.matching.band.global.util.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +13,9 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final RedisService redisService;
 
+    @Operation(hidden = true)
     @PostMapping("/signUp")
     public ApiResponse<?> signUp(@RequestBody SignUpRequest requestSignUp) {
         return ApiResponse.success();
@@ -22,6 +25,17 @@ public class UserController {
     @GetMapping("/test")
     public ApiResponse<String> test() {
         return ApiResponse.success("테스트 성공!");
+    }
+
+    @PostMapping("/redis")
+    public ApiResponse<?> redisSaveTest() {
+        redisService.saveToRedis("key", "value");
+        return ApiResponse.success();
+    }
+
+    @GetMapping("/redis")
+    public ApiResponse<String> redisGetTest() {
+        return ApiResponse.success(redisService.getFromRedis("key"));
     }
 
 }
